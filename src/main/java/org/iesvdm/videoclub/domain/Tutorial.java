93810d2a363dd2793b4da185bc7725232c757f55
darioguiles <dario.guilescuesta@hotmail.com>
2024-02-05 14:18:35 +0100

    Actualización y uso anotaciones Table y Column

---
+++ src/main/java/org/iesvdm/videoclub/domain/Tutorial.java
@@ -0,0 +1,28 @@
+package org.iesvdm.videoclub.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(
+        name = "tutorial" ,
+        schema = "videoclub_jpa",
+        indexes = {@Index(name = "titulo_index", columnList = "titulo",unique = false)}
+
+)
+public class Tutorial {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "titulo", length = 50)
+    private String titulo;
+    @Column(name = "descripcion", length = 150)
+    private String descripcion;
+    private boolean publicado;
+}
