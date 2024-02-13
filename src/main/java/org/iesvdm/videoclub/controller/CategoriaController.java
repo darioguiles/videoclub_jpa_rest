92f7a69fd50b27187ce49a9117d03e4e55374085
darioguiles <dario.guilescuesta@hotmail.com>
2024-02-13 12:27:04 +0100

    4.3 Casi acabada

---
+++ src/main/java/org/iesvdm/videoclub/controller/CategoriaController.java
@@ -0,0 +1,20 @@
+package org.iesvdm.videoclub.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.iesvdm.videoclub.service.CategoriaService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+}
