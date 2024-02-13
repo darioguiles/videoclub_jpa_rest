2e4dedce3235ffa4b71d58ce2993a62a6038c045
darioguiles <dario.guilescuesta@hotmail.com>
2024-02-13 13:00:34 +0100

    4.3 Casi acabada Falta testear

--- src/main/java/org/iesvdm/videoclub/controller/CategoriaController.java
+++ src/main/java/org/iesvdm/videoclub/controller/CategoriaController.java
@@ -1,10 +1,12 @@
 package org.iesvdm.videoclub.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.iesvdm.videoclub.domain.Categoria;
 import org.iesvdm.videoclub.service.CategoriaService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,4 +19,37 @@ public class CategoriaController {
     public CategoriaController(CategoriaService categoriaService) {
         this.categoriaService = categoriaService;
     }
+
+
+    @GetMapping({"","/"})
+    public List<Categoria> all() {
+        log.info("Accediendo a todas las películas");
+        return this.categoriaService.all();
+    }
+
+    @PostMapping({"","/"})
+    public Categoria newCategoria(@RequestBody Categoria categoria) {
+        return this.categoriaService.save(categoria);
+    }
+
+    @GetMapping("/{id}")
+    public Categoria one(@PathVariable("id") Long id) {
+        return this.categoriaService.one(id);
+    }
+
+    @PutMapping("/{id}")
+    public Categoria replaceCategoria(@PathVariable("id") Long id, @RequestBody Categoria categoria) {
+        return this.categoriaService.replace(id, categoria);
+    }
+
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteCategoria(@PathVariable("id") Long id) {
+        this.categoriaService.delete(id);
+    }
+
+
+
 }
